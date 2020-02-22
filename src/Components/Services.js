e151b0f2d28eea51ffeb341e2574213dbe7626c7
alexomosa001@gmail.com <alexomosa001@gmail.com>
2020-02-23 00:00:20 +0300

    commit

---
+++ src/Components/Services.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import Title from "../Components/Title";
+import { FaHiking, FaBeer, FaShuttleVan, FaCocktail } from "react-icons/fa";
+export default class Services extends Component {
+  state = {
+    services: [
+      {
+        icon: <FaBeer />,
+        title: "Beer",
+        info:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla profecto est, quin suam vim retineat a primo ad extremum. Et ille ridens:"
+      },
+      {
+        icon: <FaHiking />,
+        title: "Hiking",
+        info:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla profecto est, quin suam vim retineat a primo ad extremum. Et ille ridens:"
+      },
+      {
+        icon: <FaShuttleVan />,
+        title: "Pick-up Van",
+        info:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla profecto est, quin suam vim retineat a primo ad extremum. Et ille ridens:"
+      },
+      {
+        icon: <FaCocktail />,
+        title: "Cocktails",
+        info:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla profecto est, quin suam vim retineat a primo ad extremum. Et ille ridens:"
+      }
+    ]
+  };
+  render() {
+    return (
+      <secton className="services">
+        <Title title="services" />
+        <div className="services-center">
+          {this.state.services.map((item, index) => {
+            return (
+              <article key={index} className="service">
+                <span>{item.icon}</span>
+                <h6>{item.title}</h6>
+                <p>{item.info}</p>
+              </article>
+            );
+          })}
+        </div>
+      </secton>
+    );
+  }
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "../Components/Hero";
 import Banner from "../Components/Banner";
 import { Link } from "react-router-dom";
+import Services from "../Components/Services";
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
           </Link>
         </Banner>
       </Hero>
+      <Services />
     </React.Fragment>
   );
 }
